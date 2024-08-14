494386495a8d673591dbcb2e4bd7b0fd6a26dbd3
genesluna <genesluna@gmail.com>
2024-08-14 16:29:17 +0000

    feat: add router config

---
+++ src/app/app.tsx
@@ -0,0 +1,12 @@
+import { Outlet, ScrollRestoration } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Outlet />
+      <ScrollRestoration />
+    </>
+  );
+}
+
+export default App;
