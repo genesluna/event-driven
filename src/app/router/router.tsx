c73eb6635865a0fb385bd9f4306f1d9e48c8d36b
Genes Luna <genesluna@gmail.com>
2024-08-14 13:58:19 -0300

    feat: add event dashboard (#17)
    * fear: add card component
    
    * feat: add calendar component
    
    * feat: add input component
    
    * feat: add event-card
    
    * feat: add event dashboard (wip)
    
    * feat: add event dashboard route

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
+import EventsDashboard from '@/components/events-dashboard';
 import App from '@/app/app';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [],
+    children: [
+      {
+        path: '',
+        element: <EventsDashboard />,
+      },
+    ],
   },
 ]);
---
+++ src/components/event-card.tsx
@@ -0,0 +1,60 @@
+import { Building, CalendarDays, ContactRound, MapPin } from 'lucide-react';
+import { Card, CardContent, CardFooter, CardHeader } from './ui/card';
+import { Button } from './ui/button';
+
+export default function EventCard() {
+  return (
+    <Card>
+      <CardHeader>
+        <img
+          src='/src/assets/category-images/gastronomico.webp'
+          alt='event main image'
+          className='h-full w-full rounded-2xl object-cover'
+        />
+      </CardHeader>
+      <CardContent>
+        <h3 className='text-2xl font-semibold'>Event Title</h3>
+        <p className='mt-4 text-muted-foreground'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur
+          ipsum doloremque animi sit, placeat sapiente recusandae cupiditate
+          eaque velit inventore optio pariatur et provident obcaecati dolore
+          aperiam officia? Facere. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit.
+        </p>
+        <div className='mt-4 flex items-start gap-2'>
+          <div className='flex items-center gap-2'>
+            <ContactRound className='h-4 w-4 text-primary' />
+            <span className='whitespace-nowrap text-muted-foreground'>
+              Genes Luna
+            </span>
+          </div>
+          <div className='flex items-center gap-2'>
+            <span className='font-thin text-muted-foreground'>|</span>
+            <CalendarDays className='h-4 w-4 text-primary' />
+            <span className='whitespace-nowrap text-muted-foreground'>
+              21/08/2024
+            </span>
+          </div>
+          <div className='flex items-center gap-2'>
+            <span className='font-thin text-muted-foreground'>|</span>
+            <MapPin className='h-4 w-4 text-primary' />
+            <span className='whitespace-nowrap text-muted-foreground'>
+              Maceió, AL
+            </span>
+          </div>
+          <div className='flex items-center gap-2'>
+            <span className='font-thin text-muted-foreground'>|</span>
+            <Building className='h-4 w-4 text-primary' />
+            <span className='whitespace-nowrap text-muted-foreground'>
+              Shopping Maceió
+            </span>
+          </div>
+        </div>
+      </CardContent>
+      <CardFooter className='flex gap-4'>
+        <Button variant='secondary'>Participar</Button>
+        <Button variant='outline'>Detalhes</Button>
+      </CardFooter>
+    </Card>
+  );
+}
