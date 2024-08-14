056f73c73cabd8c2ede442f0a45bc97a578d80bc
Genes Luna <genesluna@gmail.com>
2024-08-14 14:46:40 -0300

    feat: add redux toolkit (#21)
    * feat: add redux store
    
    * feat: add generic slice
    
    * feat: add redux provider wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/router.tsx';
 import { createRoot } from 'react-dom/client';
+import { store } from './app/store/store.ts';
+import { Provider } from 'react-redux';
 import { StrictMode } from 'react';
 import './styles/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
