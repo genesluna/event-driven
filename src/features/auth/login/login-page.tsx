e41b77a84a6b46f7ce442cf135aa4335a4283597
genesluna <genesluna@gmail.com>
2024-08-15 14:01:44 +0000

    feat: add login page and form

---
+++ src/features/auth/login/login-page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from './components/login-form';
+
+export default function LoginPage() {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <LoginForm />
+    </div>
+  );
+}
