4038b016b844854e322bc85aab097224365b0946
genesluna <genesluna@gmail.com>
2024-08-15 14:02:00 +0000

    feat: add register page and form

---
+++ src/features/auth/register/register-page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from './components/register-form';
+
+export default function RegisterPage() {
+  return (
+    <div className='flex flex-col items-center justify-center'>
+      <RegisterForm />
+    </div>
+  );
+}
