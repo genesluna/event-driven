2dbf048cf37769756d801c42c6facd4a5e5a7ea7
genesluna <genesluna@gmail.com>
2024-08-20 12:27:21 +0000

    feat: add pt-br localization

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,10 +4,13 @@ import { logout, signIn } from '@/features/auth/auth-slice';
 import { onAuthStateChanged } from 'firebase/auth';
 import MainLayout from './layouts/main-layout';
 import { useAppDispatch } from './store/store';
+import { setDefaultOptions } from 'date-fns';
 import { auth } from './config/firebase';
+import { ptBR } from 'date-fns/locale';
 import { useEffect } from 'react';
 
 function App() {
+  setDefaultOptions({ locale: ptBR });
   const dispatch = useAppDispatch();
 
   useEffect(() => {
