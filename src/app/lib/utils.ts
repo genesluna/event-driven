1e20b5539e0a03e9cceda7d9c7c592b1faeae7f1
genesluna <genesluna@gmail.com>
2024-08-20 13:03:37 +0000

    feat: add time and date helper functions

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -4,3 +4,42 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function addTimeToDate(date: Date, timeString: string): Date {
+  const newDate = new Date(date);
+
+  const timeRegex = /^(0?[1-9]|1[0-2]):([0-5][0-9]) (AM|PM)$/i;
+  const match = timeString.match(timeRegex);
+
+  if (!match) {
+    throw new Error('Invalid format. Use "HH:MM AM/PM".');
+  }
+
+  const [, hoursStr, minutesStr, period] = match;
+  let hours = parseInt(hoursStr, 10);
+  const minutes = parseInt(minutesStr, 10);
+
+  if (period.toUpperCase() === 'PM' && hours !== 12) {
+    hours += 12;
+  } else if (period.toUpperCase() === 'AM' && hours === 12) {
+    hours = 0;
+  }
+
+  newDate.setHours(hours, minutes, 0, 0);
+
+  return newDate;
+}
+
+export function formatTime(date: Date): string {
+  let hours: number = date.getHours();
+  const minutes: number = date.getMinutes();
+  const ampm: string = hours >= 12 ? 'PM' : 'AM';
+
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  const formattedMinutes: string =
+    minutes < 10 ? '0' + minutes : minutes.toString();
+
+  const strTime: string = `${hours}:${formattedMinutes} ${ampm}`;
+  return strTime;
+}
