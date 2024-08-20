864e8b18b047c987bc49cb1c6961c845c12109d1
genesluna <genesluna@gmail.com>
2024-08-20 12:58:59 +0000

    feat: add category options

---
+++ src/features/events/form/category-options.ts
@@ -0,0 +1,9 @@
+export const categoryOptions = [
+  { text: 'Cultura', value: 'cultura' },
+  { text: 'Cinema', value: 'cinema' },
+  { text: 'Comida', value: 'comida' },
+  { text: 'Feira', value: 'feira' },
+  { text: 'Festa', value: 'festa' },
+  { text: 'Musica', value: 'musica' },
+  { text: 'Viagem', value: 'viagem' },
+];
