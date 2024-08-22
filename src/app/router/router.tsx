33dac2d77aaf0998e4e56de957bd4f26ef897fe1
genesluna <genesluna@gmail.com>
2024-08-22 13:07:10 +0000

    feat: add events routes

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,5 +1,7 @@
+import EventDetailsPage from '@/features/events/details/event-details-page';
 import EventsDashboard from '@/features/events/dashboard/events-dashboard';
 import RegisterPage from '@/features/auth/register/register-page';
+import EventForm from '@/features/events/form/envent-form';
 import LoginPage from '@/features/auth/login/login-page';
 import { createBrowserRouter } from 'react-router-dom';
 import App from '@/app/app';
@@ -9,18 +11,12 @@ export const router = createBrowserRouter([
     path: '/',
     element: <App />,
     children: [
-      {
-        path: '',
-        element: <EventsDashboard />,
-      },
-      {
-        path: 'auth/login',
-        element: <LoginPage />,
-      },
-      {
-        path: 'auth/register',
-        element: <RegisterPage />,
-      },
+      { path: '', element: <EventsDashboard /> },
+      { path: 'auth/login', element: <LoginPage /> },
+      { path: 'auth/register', element: <RegisterPage /> },
+      { path: '/events/manage/:id', element: <EventForm key='manage' /> },
+      { path: '/events/new', element: <EventForm key='create' /> },
+      { path: '/events/:id', element: <EventDetailsPage /> },
     ],
   },
 ]);
