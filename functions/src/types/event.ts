4a89fda2acd34a4d895a4e4b3f4ffbcfa293de63
genesluna <genesluna@gmail.com>
2024-08-30 23:03:59 +0000

    feat: add event write trigger function to create newsfeed

---
+++ functions/src/types/event.ts
@@ -0,0 +1,36 @@
+export type AppEvent = {
+  id: string;
+  title: string;
+  date: string;
+  time: string;
+  category: string;
+  description: string;
+  city: string;
+  venue: string;
+  coverImgURL?: string;
+  hostUid: string;
+  hostedBy: string;
+  hostPhotoURL: string;
+  isCancelled: boolean;
+  attendees: Attendee[];
+  attendeeIds: string[];
+  isHost?: boolean;
+  isGoing?: boolean;
+};
+
+export type Attendee = {
+  id: string;
+  displayName: string;
+  photoURL: string;
+};
+
+export type Comment = {
+  id: string;
+  displayName: string;
+  photoURL: string;
+  uid: string;
+  text: string;
+  date: number;
+  parentId: string | null;
+  childNodes: Comment[];
+};
